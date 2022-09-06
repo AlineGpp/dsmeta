2cfa621067aa550d59757bbc8309581c2c97d2a5
Aline <alinegoppinger.pf009@acacdemico.ifsul.edu.br>
2022-09-06 15:36:56 -0300

    first version

---
+++ backend/src/test/java/com/inehgpp/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inehgpp.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
